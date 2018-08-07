ae32acd8284339bf3fd5f75a0b36bec0079ef25f
Jesse Hoyos <jesse@mojotech.com>
2018-08-07 00:28:36 -0400

    Import scripts into index

---
+++ index.js
@@ -0,0 +1,5 @@
+import { throttle } from './thorttle';
+import { debounce } from './debounce';
+import { lerp } from './lerp';
+import { compose } from '/compose';
+import { interval } from './interval';
